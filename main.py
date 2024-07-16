22c1302f9f9fd5400568c387b7aed974a6fb01a2
Jornada de Dados <dariopereiratv@gmail.com>
2024-07-15 21:29:29 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,22 @@
+print("Jornada de Dados")
+
+#Variaveis
+idade = 34
+nome = "dario"
+
+# Print
+print(idade)
+
+# soma 
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+
+#
+lista_invertida = lista.reverse()
+
+
+
+
